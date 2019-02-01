26a05f4bd10c8ab864a41b639776f48f9b918ec1
Jarno van Lieorp <24877@ma-web.nl>
2019-02-01 14:33:24 +0100

    CoDe

---
+++ EnemyAttacks.cs
@@ -0,0 +1,28 @@
+﻿//By: Jarno van Lierop  on  Vertical Slice 2
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttacks : MonoBehaviour {
+
+    public EyeAttack eyeAttack;
+    LegAttackScript legAttackScript;
+    public GameObject TempEnemy;
+
+    private void Start()
+    {
+        legAttackScript = FindObjectOfType<LegAttackScript>();
+        legAttackScript.StartAttack();
+    }
+
+    public void StartTimer()
+    {
+        StartCoroutine(EyeAttackTimer(TempEnemy));
+    }
+
+    public IEnumerator EyeAttackTimer(GameObject TempEnemy)
+    {
+        yield return new WaitForSeconds(3.5f);
+        eyeAttack.ShootProjectile();
+    }
+}
